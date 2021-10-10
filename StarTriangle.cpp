9d2ee641483eee1d9f7ea2a3cea0300ed057ac50
retro666 <rodion.ahrameev@gmail.com>
2021-10-10 19:47:50 +0300

    Add files via upload
    Add StarTriangle

---
+++ StarTriangle.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+    unsigned int a, b = 0;
+    std::cin >> a;
+    for (; a; ++b) {
+        for (unsigned int i = a--; i > 1; --i) std::cout << ' ';
+        for (unsigned int i = 0; i < b; ++i) std::cout << "**";
+        std::cout << "*\x0A";
+    }
+    return 0;
+}
