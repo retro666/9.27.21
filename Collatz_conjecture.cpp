f653edb740413b8c82c6d7a90ea8f7e36780cd97
retro666 <rodion.ahrameev@gmail.com>
2021-10-16 15:39:39 +0300

    Домашка
    Гипотеза Коллатца;
    Подсчет целочисленных точек в окружности только целочисленными вычислениями.

---
+++ Collatz_conjecture.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main(){
+	unsigned int a;
+	std::cin >> a;
+	while (a != 4) {
+		if (a & 1) {
+			if (a >= 0x55555555) {
+				std::cerr << "OVF\n";
+				return 1;
+			}
+			std::cout << (a = a * 3 + 1);
+		}
+		else std::cout << (a >>= 1);
+		std::cout << ' ';
+	}
+	std::cout << "2 1 ...";
+	return 0;
+}
---
+++ int_in_round_without_float.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+inline unsigned int sqrti(long long a) {
+	unsigned int r = 0;
+	for (unsigned int i = 0x80000000; i; i >>= 1) {
+		if ((long long)i * i <= a - ((long long)r * i << 1)) {
+			a += (unsigned long long)r * r;
+			a -= ((unsigned long long)(r += i) * r);
+		}
+	}
+	return r;
+}
+
+int main(){
+	int a;
+	std::cin >> a;
+	if (!a) {
+		std::cout << 1;
+		return 0;
+	}
+	unsigned long long b = a;
+	for (unsigned int i = a - 1; i; i--) b += sqrti(((unsigned long long)a - i) * (a + i));
+	std::cout << (b << 2) + 1;
+	return 0;
+}
