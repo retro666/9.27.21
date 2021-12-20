8dcca15482547f1b17968d533817443c9d8b9bd1
retro666 <rodion.ahrameev@gmail.com>
2021-12-21 02:23:10 +0300

    added tests

--- int_in_round_without_float.cpp
+++ int_in_round_without_float.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
 
-inline unsigned int sqrti(long long a) {
+inline unsigned int sqrti(unsigned long long a) {
 	unsigned int r = 0;
 	for (unsigned int i = 0x80000000; i; i >>= 1) {
-		if ((long long)i * i <= a - ((long long)r * i << 1)) {
+		if ((long long)i * i <= (long long)a - ((long long)r * i << 1)) {
 			a += (unsigned long long)r * r;
 			a -= ((unsigned long long)(r += i) * r);
 		}
@@ -11,15 +11,16 @@ inline unsigned int sqrti(long long a) {
 	return r;
 }
 
-int main(){
-	int a;
-	std::cin >> a;
-	if (!a) {
-		std::cout << 1;
-		return 0;
-	}
+inline unsigned long long answer(unsigned int a) {
+	if (!a) return 1;
 	unsigned long long b = a;
 	for (unsigned int i = a - 1; i; i--) b += sqrti(((unsigned long long)a - i) * (a + i));
-	std::cout << (b << 2) + 1;
+	return (b << 2) + 1;
+}
+
+int main() {
+	unsigned int a;
+	std::cin >> a;
+	std::cout << answer(a);
 	return 0;
 }
