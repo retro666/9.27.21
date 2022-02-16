a86c1320bf394e0272b106dd02768e0cd3c844d5
retro666 <rodion.ahrameev@gmail.com>
2022-02-16 22:51:12 +0300

    Added inverse_matrix
    Removed trivial functions (create, delete matrix, etc);
    Renamed variables;
    Added templates for all functions.

--- matrix_functions.h
+++ matrix_functions.h
@@ -1,99 +1,145 @@
-template<typename T> inline T* create_matrix(size_t c, size_t r) {
-	return new T[c * r];
-}
+#pragma once
 
-template<typename T> inline void delete_matrix(T* a) {
-	delete[] a;
-}
-
-template<typename T> T* create_matrix_inited(size_t c, size_t r, T v) {
-	r *= c;
-	c = (size_t)create_matrix<T>(r, 1);
-	while (r--) ((T*)c)[r] = v;
-	return (T*)c;
-}
-
-int* create_e_matrix(size_t c, size_t r) {
-	int* a = create_matrix<int>(c, r);
-	memset(a, 0, c * r << 2);
-	for (int i = std::min(r, c++); i--;) a[i * c] = 1;
-	return a;
-}
-
-template<typename T> T* transposition_matrix(T* a, size_t r, size_t c) {
-	T* b = create_matrix<T>(r, c);
-	for (size_t i = c * r; i--;) b[i] = a[i / r + i % r * c];
-	return b;
-}
-
-template<typename T> void rotate_180deg(T* a) {
-	size_t s = _msize(a) / sizeof(T);
-	for (size_t i = s-- >> 1; i--;) std::swap(a[i], a[s - i]);
-}
-
-void mul_matrix_int(int* a, int b) {
-	for (size_t i = (_msize(a) >> 2); i--;) a[i] *= b;
-}
-
-int* create_mul_matrix_int(int* a, int b) {
-	register int d = _msize(a) >> 2;
-	int* c = new int[d];
-	while (d--) c[d] = a[d] * b;
-	return c;
-}
-
-int* create_add_matrix_matrix(int* a, int* b) {
-	register int d = _msize(a) >> 2;
-	int* c = new int[d];
-	while (d--) c[d] = a[d] + b[d];
-	return c;
-}
-
-int* create_mul_matrix_matrix(int* a, int* b, size_t ac_br, size_t ar, size_t bc) {
-	int* c = create_matrix<int>(ar, bc);
-	for (size_t i = ar; i--; ) for (size_t q = bc; q--;) {
-		c[i * bc + q] = 0;
-		for (size_t w = ac_br; w--;) 
-			c[i * bc + q] += a[i * ac_br + w] * b[w * bc + q];
+template<typename T> T* create_matrix_inited(size_t col, size_t row, T val) {
+	col = (size_t)(new T[row *= col]);
+	while (row--) {
+		((T*)col)[row] = val;
 	}
-	return c;
+	return (T*)col;
 }
 
-template<typename T> void swap_row(T* m, size_t c, size_t a, size_t b) {
-	a *= c;
-	b *= c;
-	while (c--) std::swap(m[a++], m[b++]);
+template<typename T> T* create_e_matrix(size_t col, size_t row) {
+	T* e = new T[col * row];
+	while (row--) {
+		for (size_t i = col; i--; ) {
+			e[row * col + i] = row == i;
+		}
+	}
+	return e;
 }
 
-template<typename T> void swap_column(T* m, size_t c, size_t r, size_t a, size_t b) {
-	a += c * r;
-	b += c * r;
-	while (r--) std::swap(m[a -= c], m[b -= c]);
+template<typename T> T* transposition_matrix(T* src, size_t row, size_t col) {
+	T* dst = new T[row * col];
+	for (size_t i = col * row; i--;) {
+		dst[i] = src[i / row + i % row * col];
+	}
+	return dst;
 }
 
-template<typename T> T& link_matrix_value(T* m, size_t c, size_t ac, size_t ar) {
-	return m[ar * c + ac];
+template<typename T> void rotate_180(T* matrix, size_t square) {
+	for (size_t i = square-- >> 1; i--;) {
+		std::swap(matrix[i], matrix[square - i]);
+	}
 }
 
-int det_matrix(int* m, int s) {
-	if (s > 3) {
-		int d = 0;
-		int* c = create_matrix<int>(s - 1, s - 1);
-		for (int i = s; i--;) {
-			for (int q = s - 1; q--;) {
-				for (int w = s - 1; w--;) {
-					c[w * (s - 1) + q] = m[(w + 1) * s + q + (q >= i)];
+template<typename T1, typename T2>void mul_matrix_val(T1* matrix, T2 val, size_t square) {
+	while (square--) {
+		matrix[square] *= val;
+	}
+}
+
+template<typename T1, typename T2>T1* create_mul_matrix_val(const T1* matrix, T2 val, size_t square) {
+	T1* dst = new T1[square];
+	while (square--) {
+		dst[square] = matrix[square] * val;
+	}
+	return dst;
+}
+
+template<typename T>T* create_add_matrix_matrix(const T* a, const T* b, size_t square) {
+	T* sum = new T[square];
+	while (square--) {
+		sum[square] = a[square] + b[square];
+	}
+	return sum;
+}
+
+template<typename T>T* create_mul_matrix_matrix(const T* a, const T* b, size_t a_col_b_row, size_t a_row, size_t b_col) {
+	T* dst = new T[a_row * b_col];
+	for (; a_row--; ) {
+		for (size_t q = b_col; q--;) {
+			dst[a_row * b_col + q] = 0;
+			for (size_t w = a_col_b_row; w--;)
+				dst[a_row * b_col + q] += a[a_row * a_col_b_row + w] * b[w * b_col + q];
+		}
+	}
+	return dst;
+}
+
+template<typename T> void swap_row(T* matrix, size_t col, size_t row1, size_t row2) {
+	row1 *= col;
+	row2 *= col;
+	while (col--) std::swap(matrix[row1++], matrix[row2++]);
+}
+
+template<typename T> void swap_column(T* matrix, size_t col, size_t row, size_t col1, size_t col2) {
+	col1 += col * row;
+	col2 += col * row;
+	while (row--) std::swap(matrix[col1 -= col], matrix[col2 -= col]);
+}
+
+template<typename T>T det_matrix(const T* matrix, size_t diag) {
+
+	if (diag > 3) {
+		T d = 0;
+		T* c = new T[(diag - 1) * (diag - 1)];
+		for (int i = diag; i--;) {
+			for (int q = diag - 1; q--;) {
+				for (int w = diag - 1; w--;) {
+					c[w * (diag - 1) + q] = matrix[(w + 1) * diag + q + (q >= i)];
 				}
 			};
-			d += det_matrix(c, s - 1) * (1 - ((i & 1) << 1)) * m[i];
+			d += det_matrix(c, diag - 1) * (1 - ((i & 1) << 1)) * matrix[i];
 		}
-		delete_matrix<int>(c);
+		delete[] c;
 		return d;
 	}
-	else switch (s) {
-	case 3: return m[0] * (m[4] * m[8] - m[5] * m[7]) + m[1] * (m[5] * m[6] - m[3] * m[8]) + m[2] * (m[3] * m[7] - m[4] * m[6]);
-	case 2: return m[0] * m[3] - m[1] * m[2];
-	case 1: return m[0];
+	else switch (diag) {
+	case 3: return matrix[0] * (matrix[4] * matrix[8] - matrix[5] * matrix[7]) + matrix[1] * (matrix[5] * matrix[6] - matrix[3] * matrix[8]) + matrix[2] * (matrix[3] * matrix[7] - matrix[4] * matrix[6]);
+	case 2: return matrix[0] * matrix[3] - matrix[1] * matrix[2];
+	case 1: return matrix[0];
 	default: return 1;
 	}
+}
+
+template<typename T>long double* inverse_matrix(const T* matrix, size_t diag) {
+	long double *dst = create_e_matrix<long double>(diag, diag), *src = new long double[diag * diag];
+	for (size_t i = diag * diag; i--;) src[i] = matrix[i];
+	for (size_t i = diag; i; ) {
+		if (!src[--i * (diag + 1)]) {
+			size_t q = diag;
+			while (--q && !src[q * diag + i]);
+			if (q || src[q * diag + i]) {
+				swap_row(src, diag, i, q);
+				swap_row(dst, diag, i, q);
+			}
+			else {
+				delete[] dst;
+				dst = 0;
+				goto RET;
+			}
+		}
+		for (size_t q = diag; q; --q) {
+			if ((i + 1) != q && src[i * (diag + 1) - (i - q + 1) * diag]) {
+				long double diff = (long double)src[i * (diag + 1) - (i - q + 1) * diag] / src[i * (diag + 1)];
+				for (size_t w = 0; w < diag; ++w) {
+					if (w < i) {
+						src[(q - 1) * diag + w] -= src[i * diag + w] * diff;
+					}
+					else if (w > i){
+						dst[(q - 1) * diag + w] -= dst[i * diag + w] * diff;
+					}
+				}
+				src[(q - 1) * diag + i] = 0;
+				dst[(q - 1) * diag + i] = -diff;
+			}
+		}
+	}
+	for (size_t i = diag; i--;) {
+		for (size_t q = diag; q--;) {
+			dst[i * diag + q] /= src[i * (diag + 1)];
+		}
+	}
+RET:	delete[] src;
+	return dst;
 }
